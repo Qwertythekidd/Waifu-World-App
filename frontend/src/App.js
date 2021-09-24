760d5ba999d8a9e509f46f9663ba0ebadf1258cb
Qwertythekid <Qwertythekid@gmail.com>
2021-09-24 02:17:06 -0400

    Rating Function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
-
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="index.html">Waifu World</a>
+            <a className="brand" href="index.html">
+            Waifu World
+            </a>
         </div>
         <div>
             <a href="cart.html">Cart</a>
@@ -17,30 +19,10 @@ function App() {
         <div>
         <div className="row center">
           {data.product.map(product =>(
-              <div key={product._id} className="card">
-             <a href={`/product/${product._id}`}>
-              <img 
-              className="medium" 
-              src={product.image} 
-              alt={product.name} 
-              />
-             </a>
-             <div className="card-body">
-              <a href={`/product/${product._id}`}>
-                     <h2>{product.name}</h2>
-              </a>
-                 <div className="rating">
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-              </div>
-              <div className="price">${product.price}</div>
-             </div>
-         </div>
+            <Product key={product.id} product={product}></Product>           
             ))
           }
+            }
         </div>
         </div>
     </main>
