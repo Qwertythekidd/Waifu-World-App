15274d5470d09c4b689616eacbc333b95ab76c07
Qwertythekid <Qwertythekid@gmail.com>
2021-10-16 07:35:32 -0400

    Cart Screen Created and Functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './Screens/CartScreen';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
-import CartScreen from './Screens/CartScreen';
 
 function App() {
+
+  const cart = useSelector(state => state.cart);
+  const{cartItems} = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
-      <header className="row">
-        <div>
-            <a className="brand" href="index.html">
-            Waifu World
-            </a>
-        </div>
-      <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-      </div>
-      </header>
-          <main>
-            <Route path="/cart/:id?" component={CartScreen}></Route>
-            <Route path="/product/:id" component={ProductScreen}></Route>
-            <Route path="/" component={HomeScreen} exact></Route>            
-          </main>
-        <footer className="row center">All Rights Reserved</footer>
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Waifu World
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+               Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
 }
-
 export default App;
